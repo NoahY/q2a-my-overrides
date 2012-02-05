abafaeec6195735176df29380d034d07e1a71e69
NoahY <noahy57@gmail.com>
2012-02-05 09:23:08 +0530

    fix

--- qa-my-overrides.php
+++ qa-my-overrides.php
@@ -2,7 +2,7 @@
 		
 	function qa_sanitize_html($html, $linksnewwindow=false) {
 		$safe = qa_sanitize_html_base($html, $linksnewwindow);
-		$safe = preg_replace('/denied:dpr/','dpr:',$safe);
+		$safe = preg_replace('/denied:dpr:/','dpr:',$safe);
 		return $safe;
 	}
 						
